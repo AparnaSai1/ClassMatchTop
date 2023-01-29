c56db4c8c3af7be7eb968121fe0e09e5b342ef0d
Aparna <aparna123jul@gmail.com>
2023-01-28 21:14:44 -0500

    Initial Commit

---
+++ ClassMatchTop/ClassMatchTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassMatchTopApp.swift
+//  ClassMatchTop
+//
+//  Created by Aparna Nimmagadda on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ClassMatchTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
